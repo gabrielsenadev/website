4c83f5a1f33069e822c71a89118f9cf51ea1c2b3
Gabriel Sena <git@gabrielsena.dev>
2023-05-14 22:54:42 -0300

    chore: adjust build aind eslint config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
-module.exports = {
+const nextJest = require('next/jest');
+ 
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({ dir: './' });
+ 
+// Any custom config you want to pass to Jest
+const customJestConfig = {
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['src/**/{!(layout)}*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-}
+  coveragePathIgnorePatterns : ['*\\.layout.tsx'],
+};
+ 
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+module.exports = createJestConfig(customJestConfig);
