5c3fb42dc086047bbc1ea4c4b450bd6c27972e91
Gabriel Sena <git@gabrielsena.dev>
2023-05-15 00:10:11 -0300

    test: fix tests

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -7,9 +7,4 @@ describe('Homepage', () => {
         const heading = screen.getByText(/Hello world!/i);
         expect(heading).toBeInTheDocument();
     });
-    it('should contains the heading 2', () => {
-    render(<Page />);
-        const heading = screen.getByText(/Hello world 2!/i);
-        expect(heading).toBeInTheDocument();
-    });
 });
