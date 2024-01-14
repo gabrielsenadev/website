d64d18d4a06414bbf79da2dbe72d0529f3fe186b
Gabriel Sena <git@gabrielsena.dev>
2024-01-14 10:33:07 -0300

    create a new version of the website

--- src/app/components/Welcome.tsx
+++
@@ -1,9 +0,0 @@
-
-export function Welcome() {
-  return (
-    <main className="items-center flex">
-      <h1 className='md:text-7xl text-3xl flex flex-col items-center text-black dark:text-white font-bold'>
-        <span className='text-black/70 dark:text-white/70 text-xl md:text-4xl'>Hello, I’m <span className='text-black/90 dark:text-white/90'>Gabriel Sena</span>, a</span> software engineer</h1>
-    </main>
-  )
-}
